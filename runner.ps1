61092f9da957aef18740323d3b539c1e7f5fd31a
WKoA <109245101+Reginald-Gillespie@users.noreply.github.com>
2024-06-05 11:29:29 -0500

    Update runner.ps1

--- runner.ps1
+++ runner.ps1
@@ -1,6 +1,6 @@
 # This file downloads Main.ps1 to disk (which is necessary to run itself as admin)
 #
-# Quick run command from Run dialouge: `powershell -W H -Ex B -c "iex (iwr 'url').Content`
+# Quick run command from Run dialouge: `powershell -W H -Ex B -c "iex (iwr 'https://raw.githubusercontent.com/Reginald-Gillespie/UCAAIW/main/runner.ps1').Content`
 #
 
 # Save to disk
