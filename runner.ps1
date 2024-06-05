886bfcef51d78a41869e01547632271b7eb180d8
WKoA <109245101+Reginald-Gillespie@users.noreply.github.com>
2024-06-05 11:29:03 -0500

    Create runner.ps1

---
+++ runner.ps1
@@ -0,0 +1,13 @@
+# This file downloads Main.ps1 to disk (which is necessary to run itself as admin)
+#
+# Quick run command from Run dialouge: `powershell -W H -Ex B -c "iex (iwr 'url').Content`
+#
+
+# Save to disk
+$scriptURL = "https://raw.githubusercontent.com/Reginald-Gillespie/UCAAIW/main/Main.ps1"
+$saveLoc = "C:\Temp\Main.ps1"
+Invoke-WebRequest -Uri $scriptURL -OutFile $saveLoc
+
+# Start
+Invoke-Expression -Command $saveLoc
+
